94ee41eb1067403d74e769cbf60f78af61959063
renya_wa <151263469+corvula@users.noreply.github.com>
2025-09-18 15:49:31 +0300

    Update Medication.java

--- src/ua/model/Medication.java
+++ src/ua/model/Medication.java
@@ -1,1 +1,23 @@
+package ua.model;
 
+import ua.util.ValidationHelper;
+
+public class Medication {
+    private String name;
+    private String dosage;
+    private String instructions;
+
+    public Medication(String name, String dosage, String instructions) {
+        ValidationHelper.requireNonEmpty(name, "Medication Name");
+        ValidationHelper.requireNonEmpty(dosage, "Dosage");
+        ValidationHelper.requireNonEmpty(instructions, "Instructions");
+        this.name = name;
+        this.dosage = dosage;
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + dosage + ") - " + instructions;
+    }
+}
