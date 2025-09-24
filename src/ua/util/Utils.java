3ff83039384c263fc17bd9bd95b4acdd27c052de
renya_wa <151263469+corvula@users.noreply.github.com>
2025-09-24 15:01:29 +0300

    Update Utils.java

--- src/ua/util/Utils.java
+++ src/ua/util/Utils.java
@@ -1,1 +1,28 @@
+package ua.util;
 
+import ua.model.Person;
+import ua.model.Student;
+import ua.model.Teacher;
+
+public class Utils {
+    public static Person createPerson(String name, int age) {
+        if (!ValidationHelper.isValidName(name) || !ValidationHelper.isValidAge(age)) {
+            throw new IllegalArgumentException("Invalid person data!");
+        }
+        return new Person(name, age);
+    }
+
+    public static Student createStudent(String name, int age, int grade) {
+        if (!ValidationHelper.isValidGrade(grade)) {
+            throw new IllegalArgumentException("Invalid grade!");
+        }
+        return new Student(name, age, grade);
+    }
+
+    public static Teacher createTeacher(String name, int age, String subject) {
+        if (!ValidationHelper.isValidName(name)) {
+            throw new IllegalArgumentException("Invalid teacher name!");
+        }
+        return new Teacher(name, age, subject);
+    }
+}
