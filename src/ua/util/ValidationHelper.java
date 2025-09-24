ef0593743f3b756fdccde8a2b70d3b2499ca0987
renya_wa <151263469+corvula@users.noreply.github.com>
2025-09-24 15:01:10 +0300

    Update ValidationHelper.java

--- src/ua/util/ValidationHelper.java
+++ src/ua/util/ValidationHelper.java
@@ -1,1 +1,15 @@
+package ua.util;
 
+class ValidationHelper {
+    static boolean isValidName(String name) {
+        return name != null && name.matches("[A-Z][a-zA-Z]{1,20}");
+    }
+
+    static boolean isValidAge(int age) {
+        return age >= 0 && age <= 120;
+    }
+
+    static boolean isValidGrade(int grade) {
+        return grade >= 1 && grade <= 12;
+    }
+}
