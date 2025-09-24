3753b62b008e6f8e2206dda244e107ecef90eb9f
renya_wa <151263469+corvula@users.noreply.github.com>
2025-09-24 15:02:07 +0300

    Update Person.java

--- src/ua/model/Person.java
+++ src/ua/model/Person.java
@@ -1,1 +1,49 @@
 
+package ua.model;
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        if (name == null || name.length() < 2) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+        this.name = name;
+    }
+
+    public int getAge() { return age; }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Invalid age");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person{name='%s', age=%d}", name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
