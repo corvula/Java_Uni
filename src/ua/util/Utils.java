70a3431d911f5c3949f4aca1613b38c22ac2490f
renya_wa <151263469+corvula@users.noreply.github.com>
2025-10-10 13:07:23 +0300

    Update Utils.java

--- src/ua/util/Utils.java
+++ src/ua/util/Utils.java
@@ -2,10 +2,12 @@ package ua.util;
 
 import ua.model.Patient;
 import ua.model.Doctor;
+import ua.model.enums.Specialty;
 
 import java.time.LocalDate;
 
 public class Utils {
+
     public static Patient createPatient(String firstName, String lastName, String patientId, LocalDate birthDate) {
         if (!ValidationHelper.isValidId(patientId)) {
             throw new IllegalArgumentException("Invalid patient ID");
@@ -13,7 +15,27 @@ public class Utils {
         return new Patient(firstName, lastName, patientId, birthDate);
     }
 
-    public static Doctor createDoctor(String firstName, String lastName, String specialty) {
+    public static Doctor createDoctor(String firstName, String lastName, String specialtyName) {
+        Specialty specialty;
+
+        specialty = switch (specialtyName.toLowerCase()) {
+            case "cardiologist" -> Specialty.CARDIOLOGIST;
+            case "dermatologist" -> Specialty.DERMATOLOGIST;
+            case "pediatrician" -> Specialty.PEDIATRICIAN;
+            case "neurologist" -> Specialty.NEUROLOGIST;
+            default -> Specialty.GENERAL_PRACTITIONER;
+        };
+
         return new Doctor(firstName, lastName, specialty);
+    }
+
+    public static String getSpecialtyDescription(Specialty specialty) {
+        return switch (specialty) {
+            case GENERAL_PRACTITIONER -> "Provides general health care.";
+            case CARDIOLOGIST -> "Specializes in heart diseases.";
+            case DERMATOLOGIST -> "Treats skin conditions.";
+            case PEDIATRICIAN -> "Treats children.";
+            case NEUROLOGIST -> "Focuses on the nervous system.";
+        };
     }
 }
